66a7f8f75ca789debccff1b450773197815b230f
DhimantBhardwaj24BAI10746 <dhimant.24bai10746@vitbhopal.ac.in>
2026-03-26 18:45:03 +0530

    Update script1_system_identity.sh

--- script1_system_identity.sh
+++ script1_system_identity.sh
@@ -1,9 +1,7 @@
----
-### **Script 1 — System Identity Report (Dhimant)**
-```bash
 #!/bin/bash
-# Script 1: System Identity Report
-# Author: Dhimant Bhardwaj | Course: Open Source Software
+# System Identity Report
+# Author: Dhimant Bhardwaj
+# Course Project: Open Source Software Audit
 # Project Software: Git
 
 STUDENT_NAME="Dhimant Bhardwaj"
@@ -13,24 +11,24 @@ KERNEL_VERSION=$(uname -r)
 LOGIN_USER=$(whoami)
 USER_HOME=$HOME
 SYSTEM_UPTIME=$(uptime -p)
-NOW=$(date "+%A, %d %B %Y %I:%M:%S %p")
+NOW=$(date "+%A, %d %B %Y | %I:%M:%S %p")
 
 if [ -f /etc/os-release ]; then
     DISTRO_NAME=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
 else
-    DISTRO_NAME="Unknown"
+    DISTRO_NAME="Unknown Linux Distribution"
 fi
 
-echo "**********************************************"
-echo " Open Source Audit - $STUDENT_NAME"
-echo "**********************************************"
+echo "=================================================="
+echo "     Open Source Audit - $STUDENT_NAME"
+echo "=================================================="
 echo "Selected Software : $SOFTWARE_CHOICE"
-echo "Distribution      : $DISTRO_NAME"
-echo "Kernel            : $KERNEL_VERSION"
+echo "Linux Distribution: $DISTRO_NAME"
+echo "Kernel Version    : $KERNEL_VERSION"
 echo "Logged-in User    : $LOGIN_USER"
 echo "Home Directory    : $USER_HOME"
-echo "Uptime            : $SYSTEM_UPTIME"
+echo "System Uptime     : $SYSTEM_UPTIME"
 echo "Date and Time     : $NOW"
 echo "Linux License     : GPL v2 (Linux kernel)"
-echo "**********************************************"
-echo "This system runs on open foundations built by global communities."
+echo "=================================================="
+echo "Git reflects the open-source spirit by enabling transparent and collaborative version control."
